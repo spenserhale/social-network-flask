595ad13a306cbade8710f8ebb59a76695b2ec9ce
Spenser Hale <spenserhale@outlook.com>
2016-06-19 19:55:49 -0700

    creating register router that creates a User model on post if form is validated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, g
+from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask.ext.login import LoginManager
 
+import forms
 import models
 
 DEBUG = True
@@ -35,6 +36,20 @@ def after_request(response):
     """Close the database connection after each request."""
     g.db.close()
     return response
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash("Yay, you registered!", "success")
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
