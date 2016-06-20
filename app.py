cf3a3559338ccb33d9fa2cd74abdb8db2f8aa586
Spenser Hale <spenserhale@outlook.com>
2016-06-19 22:34:35 -0700

    added stream view with routes of /stream and /stream<username>

--- app.py
+++ app.py
@@ -99,6 +99,21 @@ def index():
     return render_template('stream.html', stream=stream)
 
 
+@app.route('/stream')
+@app.route('/stream/<username>')
+def stream(username=None):
+    template = 'stream.html'
+    if username and username != current_user.username:
+        user = models.User.select().where(models.User.username ** username).get()
+        stream = user.posts.limit(100)
+    else:
+        stream = current_user.get_stream().limit(100)
+        user = current_user
+    if username:
+        template = 'user_stream.html'
+    return render_template(template, stream=stream, user=user)
+
+
 if __name__ == '__main__':
     models.initialize()
     try:
