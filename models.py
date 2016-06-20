46e6bf6596a9cd2acd5562a98b78e906ed0328e7
Spenser Hale <spenserhale@outlook.com>
2016-06-19 19:10:05 -0700

    imported generate_password_hash from bcrypt and created create_user class method

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from flask.ext.bcrypt import generate_password_hash
 from flask.ext.login import UserMixin
 from peewee import *
 
@@ -16,3 +17,14 @@ class User(UserMixin, Model):
     class Meta:
         database = DATABASE
         order_by = ('-joined_at',)
+
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin)
+        except IntegrityError:
+            raise ValueError("User already exists")
