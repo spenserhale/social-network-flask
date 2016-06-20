9d72a35bda612421aced483a25d130d1988e27e8
Spenser Hale <spenserhale@outlook.com>
2016-06-19 19:40:18 -0700

    imported the LoginManager class and created a load_user method for login manager to try a fetch a user from db. Also created a db initializer function to setup user table. And created myself a user on start up.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, g
+from flask.ext.login import LoginManager
 
 import models
 
@@ -7,6 +8,19 @@ PORT = 8000
 HOST = '0.0.0.0'
 
 app = Flask(__name__)
+app.secret_key = 'BH+>,wTatAj/~&-|HsQA?!0W|sqDLI:Fl)5vq8m]a3c(|Dj#bL=Z AC(8N3+Zdg>'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 
 @app.before_request
@@ -24,4 +38,11 @@ def after_request(response):
 
 
 if __name__ == '__main__':
+    models.initialize()
+    models.User.create_user(
+        username='spenserhale',
+        email='spenser.hale.accounts@outlook.com',
+        password='temp_password',
+        admin=True
+    )
     app.run(debug=DEBUG, host=HOST, port=PORT)
